f251c9197b7f8ce18c0036df7a8589b601a47824
Esther J Iyege <77330706+Faithesther20@users.noreply.github.com>
2022-02-16 15:22:08 +0100

    login

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,34 @@
 import React from "react";
 import Login from "./Login";
 
-var isLoggedIn = true;
+var isLoggedIn = false;
+var currentTime = new Date(2022, 2, 1, 15).getHours();
 
-function renderConditionally() {
-  if (isLoggedIn === true) {
-    return <h1>Hello</h1>;
-  } else {
-    return <Login />;
-  }
-}
+// function renderConditionally() {
+//   if (isLoggedIn === true) {
+//     return <h1>Hello</h1>;
+//   } else {
+//     return <Login />;
+//   }
+// }
 
 function App() {
-  return <div className="container">{renderConditionally()}</div>;
+  return (
+    <div className="container">
+      {
+        // {renderConditionally()}
+        // <h1>Hello</h1>
+        // Using tenary Operator
+        isLoggedIn ? <h1>Hello</h1> : <Login />
+
+        //Using && in react
+        //currentTime > 12 && <h1>You are still working :)</h1>
+        // for later refrence I understand that in react when using
+        // the && it would run the statment that is attached to it if
+        // that statment is true and contrary if its false
+      }
+    </div>
+  );
 }
 
 export default App;
